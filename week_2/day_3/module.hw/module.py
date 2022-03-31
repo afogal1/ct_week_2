0726d649da4e274c1964a2ca55d71df522dabdb1
Alexander Fogal <92050510+afogal1@users.noreply.github.com>
2022-03-31 16:36:07 -0500

    Delete week_2 directory

--- week_2/day_3/module.hw/module.py
+++
@@ -1,8 +0,0 @@
-from math import pi
-def squareFeet(length,width):
-    area = length * width
-    print(f"{length} ft x {width} ft = {area} sq.ft.")
-
-def circumf(dia):
-    circum = round(dia * pi)
-    print(f"Circumference of {dia} in. diameter = {circum} in.")
